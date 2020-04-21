68f20fa8cefef5e845f6ff55f787ce0106a139be
abrelle <46755278+abrelle@users.noreply.github.com>
2020-04-21 09:55:13 +0300

    First commit

---
+++ src/POP3Client.java
@@ -0,0 +1,247 @@
+import com.sun.org.apache.bcel.internal.generic.POP;
+
+import javax.net.SocketFactory;
+import javax.net.ssl.SSLSocketFactory;
+import java.io.*;
+import java.net.*;
+import java.util.Vector;
+
+
+public class POP3Client {
+
+    // private String host = "pop.gmail.com";
+
+    public static final int DEFAULT_SSL_PORT = 995;
+    public static final int DEFAULT_PORT = 110; //not used right now, but is in RFC
+    private String username;
+    private String password;
+    private Socket socketSSL = null;
+    private String host;
+    private BufferedReader reader;
+    private BufferedWriter writer;
+
+
+    POP3Client(String host, int port) throws Exception {  //not used right now, only 995 port
+
+        if(port != DEFAULT_PORT && port != DEFAULT_SSL_PORT){
+            System.out.println("Wrong port number.");
+            throw new Exception("PORT error.");
+        }
+        SocketFactory socketFactory = SSLSocketFactory.getDefault();
+        socketSSL = socketFactory.createSocket(host, port);
+        reader = new BufferedReader(
+                new InputStreamReader(socketSSL.getInputStream()));
+        writer = new BufferedWriter(
+                new OutputStreamWriter(socketSSL.getOutputStream()));
+        checkServer();
+    }
+
+    POP3Client(String host) throws Exception {
+
+        SocketFactory socketFactory = SSLSocketFactory.getDefault();
+        socketSSL = socketFactory.createSocket(host, DEFAULT_SSL_PORT);
+        reader = new BufferedReader(
+                new InputStreamReader(socketSSL.getInputStream()));
+        writer = new BufferedWriter(
+                new OutputStreamWriter(socketSSL.getOutputStream()));
+        try {
+            checkServer();
+        }
+        catch(Exception e){
+            throw e;
+        }
+    }
+
+
+    public void checkServer() throws Exception {        //prints if connection is established
+        String response = reader.readLine();
+        if(response.startsWith("+OK"))
+            System.out.println("Connected to POP3 server successfully.");
+        else
+            throw new Exception("Could not connect to server");
+    }
+
+    public boolean is_connected() {
+        return this.socketSSL != null && this.socketSSL.isConnected();
+    }
+
+    public void disconnect() throws IOException {
+        if (this.is_connected()){
+            this.socketSSL.close();
+            reader = null;
+            writer = null;
+        }
+
+        System.out.println("You are disconnected.");
+    }
+
+
+
+    public boolean login(String usr, String pass) throws IOException {
+        try{
+            this.user(usr);
+            this.pass(pass);
+            return true;
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return false;
+        }
+
+    }
+
+
+    private String sendCommand(String command) throws IOException {
+        writer.write(command + "\n");
+        writer.flush();
+        String response = reader.readLine();
+       System.out.println(response);
+        return response;
+    }
+
+
+//---------------------------------------------------------------------------------------------------------------------
+    public void user(String usr) throws Exception {   //USER command passes username and checks if +OK or -ERR
+        String response = sendCommand("USER " + usr);
+        /*if(response.startsWith("+OK"))
+            System.out.println("Correct username.");   //pointless part
+        else
+            throw new Exception("Wrong username.");*/
+
+    }
+
+    public void pass(String pass) throws Exception {   //PASS command passes password and checks if +OK or -ERR
+        String response = sendCommand("PASS " + pass);
+        if(response.startsWith("+OK"))
+            System.out.println("Correct password.");
+        else
+            throw new Exception("Wrong password.");
+
+    }
+
+    public void stat() throws IOException{      //STAT command returns 1)+OK [number of mails] [number of octets], 2) -ERR
+
+        String response =  sendCommand("Stat");
+        String [] args;
+        if(response.startsWith("+OK")) {
+            args = response.split(" ", 0);   //parse string into three parts args[] = {"+ok", "number of mails", "number of octets"}
+            System.out.println("Number of mails: " + args[1]);
+        }
+        else
+           System.out.println("Could not get number of mails.");
+
+    }
+
+    public void quit() throws IOException {
+
+            String response;
+            response = sendCommand("QUIT");
+            if(response.startsWith("+OK"))
+                System.out.println("Logged out.");
+            else
+                System.out.println("Haven't logged out.");
+            System.out.println(response);
+
+    }
+
+    public void noop() throws IOException {   //NOOP basically does nothing, returns +OK or -ERR
+        String response;
+        response = sendCommand("NOOP");
+        System.out.println(response);
+    }
+
+    public void delete(int num) throws IOException {  //DELE command marks message as deleted, but does not delete it until UPDATE state
+                                                       // returns +OK if successful, -ERR if not
+        if(num > 0){
+            String response;
+            response = sendCommand("DELE " + num);
+            if(response.startsWith("+OK"))
+                System.out.println("Message marked as deleted.");
+            else
+                System.out.println("Message could not be deleted.");
+                System.out.println(response);
+        }
+        else
+            System.out.println("Wrong number input");
+
+    }
+
+    public void reset() throws IOException {    //RSET command - any message marked as deleted is unmarked  +OK if success, -ERR otherwise
+        String response;
+        response = sendCommand("RSET");
+        if(response.startsWith("+OK"))
+            System.out.println("All messages are unmarked.");
+        else
+            System.out.println("Could not reset messages.");
+    }
+
+    private Vector<String> getMultiLines(String command) {
+        Vector<String> lines = new Vector<String>();
+        try {
+            String result = sendCommand(command);
+            lines.addElement(result);
+
+            if (!result.startsWith("+OK")){
+                System.out.println(result);
+                return lines;                          //single line
+            }
+
+            boolean ifLinesLeft = true;
+            String newLine = null;
+            while (ifLinesLeft) {
+                newLine = reader.readLine();
+                lines.addElement(newLine);
+
+                if (".".equals(newLine)) {//when sending multi lines the last line is CRLF.CRLF
+                    ifLinesLeft = false;
+                    return lines;
+                }
+                else
+                    System.out.println(newLine);
+
+            }
+
+        } catch (Exception e) {
+        }
+        return lines;
+    }
+
+    public void retrieveMsg(int num) throws IOException {
+        if(num > 0){
+           Vector<String> multi = getMultiLines("RETR " + num);
+        }
+        else
+            System.out.println("Wrong number input");
+    }
+
+    public void list() throws IOException {
+        Vector<String> multi = getMultiLines("LIST");
+
+    }
+
+    public void top(int message, int lines){
+        if(lines > 0 && message > 0){
+            Vector<String> multi = getMultiLines("TOP " + message + " " + lines);
+        }
+        else
+            System.out.println("Wrong number input");
+    }
+
+    public void uidl(int num) throws IOException {
+        if(num == 0){
+            Vector<String> multi = getMultiLines("UIDL");
+        }
+        else if(num > 0){
+            String response = sendCommand("UIDL " + num);
+            String [] args;
+            if(response.startsWith("+OK")) {
+                args = response.split(" ", 0);   //parse string into three parts args[] = {"+ok", "index", "unique id"}
+                System.out.println("Unique ID of a message: " + args[2]);
+            }
+            else
+                System.out.println("Could not get unique ID of a message.");
+        }
+        else
+            System.out.println("Wrong number input");
+    }
+
+}
---
+++ src/testPOP3Client.java
@@ -0,0 +1,152 @@
+import java.awt.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+/**state values:
+ * 0 - nothing
+ * 1 - authentication
+ * 2 - transaction
+ */
+
+public class testPOP3Client{
+
+    public static void main(String[] args) throws IOException{
+        int state = 0;
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        String host = "pop.gmail.com";
+        try {
+            POP3Client pop3Client = new POP3Client(host);
+            state = 1;
+            while(state == 1){
+                System.out.println("Enter your username:");
+                String username = in.readLine();
+                System.out.println("Enter your password:");
+                String password = in.readLine();
+                if(pop3Client.login(username, password)){
+                    System.out.println("You have logged in.");
+                    state = 2;
+                }
+            }
+
+            while(state == 2){
+                System.out.println("Press key and enter to choose an option.\n\n");
+                System.out.println("1. Read a message.");
+                System.out.println("2. Mark a message as deleted.");
+                System.out.println("3. List all messages.");
+                System.out.println("4. Total number of messages.");
+                System.out.println("5. Reset all messages marked as deleted.");
+                System.out.println("6. Choose a message and select number of lines to read.");
+                System.out.println("7. Unique-id of a message.");
+                System.out.println("8. Exit.");
+                String keyboardInput = in.readLine();
+                if(!Character.isLetter(keyboardInput.charAt(0))){
+                    int  choice = getNumber(keyboardInput);
+                    int userNumberInt = 0;
+                    //System.out.println(choice);
+                    String userNumberInput = null;
+                    switch(choice){
+                        case 1:{
+                            System.out.println("You selected to read a message. Please enter a number of selected message:");
+                            userNumberInput = in.readLine();
+                            userNumberInt = getNumber(userNumberInput);
+                            if(userNumberInt != -1)
+                                pop3Client.retrieveMsg(userNumberInt);
+                            System.out.println("Press any key and enter to continue.");
+                            String wait = in.readLine();
+                            break;
+                        }
+                        case 2:{
+                            System.out.println("You have selected to delete an email. Please enter number of an email:");
+                            userNumberInput = in.readLine();
+                            userNumberInt = getNumber(userNumberInput);
+                            if(userNumberInt != -1)
+                                pop3Client.delete(userNumberInt);
+                            System.out.println("Press any key and enter to continue.");
+                            String wait = in.readLine();
+                            break;
+                        }
+                        case 3:{
+                            System.out.println("List of emails.");
+                            pop3Client.list();
+                            System.out.println("Press any key and enter to continue.");
+                            String wait = in.readLine();
+                            break;
+                        }
+                        case 4:{
+                            pop3Client.stat();
+                            System.out.println("Press any key and enter to continue.");
+                            String wait = in.readLine();
+                            break;
+                        }
+                        case 5:{
+                            System.out.println("You have selected to reset all emails marked as deleted.");
+                            pop3Client.reset();
+                            System.out.println("Press any key and enter to continue.");
+                            String wait = in.readLine();
+                            break;
+                        }
+
+                        case 6:{
+                            System.out.println("Which message would you like to read:");
+                            userNumberInput = in.readLine();
+                            userNumberInt = getNumber(userNumberInput);
+
+                            System.out.println("How many lines of body would you like to read:");
+                            String userNumberInput2 = in.readLine();
+                            int userNumberInt2 = getNumber(userNumberInput);
+
+                            if(userNumberInt != -1 && userNumberInt2 != -1)
+                                pop3Client.top(userNumberInt, userNumberInt2);
+
+                            System.out.println("Press any key and enter to continue.");
+                            String wait = in.readLine();
+                            break;
+                        }
+
+                        case 7:{
+                            System.out.println("Type a number of a message and press enter. For all messages type 0:");
+                            userNumberInput = in.readLine();
+                            userNumberInt = getNumber(userNumberInput);
+                            if(userNumberInt != -1)
+                                pop3Client.uidl(userNumberInt);
+                            System.out.println("Press any key and enter to continue.");
+                            String wait = in.readLine();
+                            break;
+                        }
+
+                        case 8:{
+                            System.out.println("You have selected to quit.");
+                            pop3Client.quit();
+                            pop3Client.disconnect();
+                            System.out.println("Goodbye.");
+                            System.exit(0);
+                        }
+
+                        default:{
+                            System.out.print("Error.Try again.");
+                        }
+                    }
+                }
+            }
+
+        } catch(Exception out){
+            System.out.println(out.getMessage());
+        }
+
+    }
+
+    private static int getNumber(String str){
+        try {
+            int temp = Integer.parseInt(str);
+            return temp;
+        }
+        catch (NumberFormatException e)
+        {
+            System.out.println("Wrong input. Try Again.");
+            return -1;
+        }
+    }
+
+}
